e4751364851c8ddae3b64059f74bf80b9869c0c1
Tuan Nguyen <tuan.nat915@gmail.com>
2023-11-15 14:54:16 -0500

    styling navbar

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,34 @@
+// node packages
+import { NavLink } from 'react-router-dom';
+// css
+import styles from './NavBar.module.css'
+// logo
+import TuanSign from '../../assets/images/Tuan_signature.png'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <div>
+                <img src={TuanSign} alt="Tuan Signature"  className={styles.navBarLogo} />
+            </div>
+            <div>
+                <ul className={styles.navBarElements}>
+                    <li>
+                        <NavLink to='/'>Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/project'>Project</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/about'>About</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to='/contact'>Contact</NavLink>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,10 @@
+import NavBar from "../../components/NavBar/NavBar"
+
+
 const Home = () => {
     return (
         <>
+            <NavBar />
             <h1>This is homepage</h1>
         </>
     )
